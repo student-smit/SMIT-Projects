0a050eb3c03c63e3cdad89950f4b3290629c08b2
student-smit <mosyyebshah1@gmail.com>
2026-03-31 21:16:59 +0500

    add project rivision class 03

---
+++ rivision class 03 sir hassan/app.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+// for each kuch return nahi karta 
+//  map return kerta hai 1 array 
+// filter return kerta hai 1 array 
+//  or find ye first  value return karta hai jab condition true hogti hai 
+
+let arr = [1 , 2 , 3, 4 , 5 ,6 ,7];
+
+
+//  =============foreach ========================
+
+// arr.forEach((data) => {console.log(data)})
+
+// ==================== map =================================
+
+// let array = arr.map((data) =>{
+// return data;
+
+// }) 
+// console.log(array);
+
+
+
+// ================ filter =================================
+
+// let filterItems = arr.filters((item) => item > 2)
+// console.log(filterItems)
+
+
+// for (let i = 0 ; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+
+//  ==================== find  ==============================
+
+// let findReturn = arr.find((item) => item === 4)
+// console.log(findReturn)
