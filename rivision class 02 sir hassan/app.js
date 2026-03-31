befc0ceb3c03ed8a4e86456643ca6f30051ea4a7
student-smit <mosyyebshah1@gmail.com>
2026-03-31 20:09:15 +0500

    add project rivision class 02

---
+++ rivision class 02 sir hassan/app.js
@@ -0,0 +1,36 @@
+// `` backtick ka kam dynamic string banana hai 
+
+
+
+
+// var username;
+
+// // console.log(username)
+
+// if(username){
+//     console.log("if true hogiaa ")
+// }else {
+//     console.log("else block chlaa ==>")
+// }
+
+
+// example 
+
+
+// let firtsName = 'musayyab';
+// let lastName = 'shah';
+
+// let fullName = firtsName + " " + lastName ;  method one
+// let fullName = firtsName.concat(lastName);   method second
+// let fullName = `${firtsName} ${lastName}`    method three
+// console.log(fullName);
+
+
+// example 
+
+
+// let result = 2 + 2 + 1872837
+// console.log(result);
+
+
+
