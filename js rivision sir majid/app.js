85b38f66ccbcad9027b08e4d869e356f0f44fa3a
student-smit <mosyyebshah1@gmail.com>
2026-04-09 18:55:00 +0500

    Add project javascript rivision

---
+++ js rivision sir majid/app.js
@@ -0,0 +1,124 @@
+// var myFav = 'red';
+
+// console.log(myFav);
+
+
+// ================================= 1
+
+// hello()
+
+// function hello(){
+// console.log('ooo behtreen' );
+
+// }
+
+// ======================================= 2
+
+
+// hello()
+
+// var hello = () => {
+//     console.log('ooo shikari');
+    
+// }
+
+
+//  =========================3
+
+// function newFunc  ()  {
+//     let user = 'Saylani'
+// };
+
+// newFunc();
+
+// console.log(user)
+
+
+// block scope ||   local scope  ||  function scope
+
+
+// let test1 = () => {
+//     var user = 1 
+//     console.log(user);
+      
+// }
+
+
+// test1()
+
+
+// let test2 = () => {
+//     var user = 2 
+//     console.log(user);
+      
+// }
+
+// test2()
+
+
+
+// let test3 = () => {
+//     var user = 3
+//     console.log(user);
+      
+// }
+
+
+// test3()
+
+// ============================================================================
+
+
+
+// for(let i = 0 ; i < 5 ; i++){
+//    console.log('han bhi ustad');
+   
+// }
+
+
+// while loop 
+
+// let  i = 1 ;
+
+// while (i <= 5){
+//     alert(i)
+//     i++
+//  }
+
+
+
+// let i = 10;
+
+// do {
+//     alert(i);
+//     i++;
+// }
+
+// while(i <= 5);
+
+
+
+
+
+
+// ========================================================
+
+
+
+// if (document.getElementById(fieldId).value === ""){
+//     alert("Email address required.");
+// }
+
+
+// document.getElementById(fieldId).value === "" ? alert('email required') : alert('form submited')
+
+// document.getElementById(fieldId).value === "" && alert('email required');
+
+// document.getElementById(fieldId).value !== "" alert('form submitted')
+
+// var myName = 'majid';
+
+// myName = '';
+
+// !myName 
+
