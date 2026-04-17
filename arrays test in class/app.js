ef20779737fe2d653e5248a40ce20adcd3c70eb8
student-smit <mosyyebshah1@gmail.com>
2026-04-17 17:01:15 +0500

    add project array method

---
+++ arrays test in class/app.js
@@ -0,0 +1,184 @@
+// ================================ section 1 ===================== 
+
+// ans  1
+// new  array 
+
+
+
+
+// ans 2
+// selecting element  based on condition 
+
+
+// ans 3 
+// single value 
+
+
+// ans 4 
+// find
+
+
+// ans 5
+// index of
+
+
+// =========================== section 2 =====================
+
+
+// ans 1 
+//  2 , 4 , 6 ,8
+
+
+// ans 2 
+// 15 
+
+// ans 3
+// 6
+
+
+
+
+// ================================== section 3 =====================
+
+
+// ========================================================
+
+// let array = [ 1 , 2 , 3 ,4];
+// let result = array.map(( data) => (data**2))
+// console.log(result);
+
+
+// ans 1 
+// [1, 4, 9, 16]
+
+// =============================================================
+
+
+// let filter = [ 1 , 2 , 3 ,4 , 5 ,6 ,7 ,8];
+// let result = filter.filter(( data) => (data % 2 === 0))
+// console.log(result);
+
+
+// ans 2 
+// [2, 4, 6, 8]
+
+// ===============================================================
+
+// let arr = [1 ,2 ,3 ,4, 5, 6, 7,8];
+// let sum = arr.reduce((acc , current ) => ( acc + current))
+    
+// console.log(sum);
+    
+    // ans 3
+    // 36
+
+// ========================================================
+
+
+    // let arr = [ 1 , 2, ,3 ,4 ,5 ];
+    // let find = arr.find((data) => (data) > 3)
+    // console.log(find);
+    
+
+    // ans 4
+    // 4
+    
+
+//  =============================================================
+
+// let arr = [ 1, 3, 5, 9, 2, 8, 8, ]
+// let findIndex =  arr.findIndex()
+// console.log(findIndex);
+
+
+
+// ==================================================================
+
+// let arr1 = [1 ,2 ] ;
+// let arr2 = [3 , 4 ];
+
+// let murge = arr1.concat(arr2)
+// console.log(murge);
+
+// ans 6 
+// [1, 2, 3, 4]
+
+
+// =======================================================
+
+// let arr = ['hello','world'];
+// console.log(arr);
+
+// let result = arr.join('')
+// console.log(result);
+
+
+// ans 7 
+// helloworld
+
+// ==========================================
+
+// let arr = [1 ,9 ,3 , 5, 4, 6, 7, ];
+// let assending = arr.sort((a,b) => a-b  )
+// console.log(assending);
+
+
+// ans 8
+// [1, 3, 4, 5, 6, 7, 9]
+
+
+// =========================================
+
+// let arr = [1 , 2, 4, 5];
+// let reverse = arr.reverse()
+// console.log(reverse);
+
+// ans 9
+// [5, 4, 2, 1]
+
+
+// =====================================================
+
+// =============================== section 4 ===========================
+
+
+// let students = [
+
+// {    name: 'ali',marks:80},
+// {     name : 'sara ' , marks :45 },
+// {  name : "ahmed" , marks :70}
+
+// ]
+
+// ans 1
+
+// let filter = students.filter((s) => s.marks > 50)
+// console.log(filter);
+
+
+
+
+
+// ans 2
+
+// let names  = students.map((s) => s.name)
+// console.log(names);
+
+// ans 3
+
+// let marks = students.reduce((Acc,curr) => Acc + curr.marks,0); 
+// console.log(marks);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
