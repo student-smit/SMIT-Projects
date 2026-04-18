c2d58331369fd1fe905a1c043829973f3196eff5
student-smit <mosyyebshah1@gmail.com>
2026-04-18 11:53:29 +0500

    add project calculator in DOM

---
+++ calculator in dom/app.js
@@ -0,0 +1,26 @@
+var input = document.getElementById('inp')
+
+function calculate(e){
+
+
+    var val = input.value = input.value + e;
+
+
+}
+
+function equal (){
+    input.value = eval(input.value);
+}
+
+function reset(){
+     input.value = "";
+}
+
+
+
+
+
+
+
+
+
